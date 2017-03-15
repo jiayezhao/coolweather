8df8bc05343267c4c80a7dccfb9e7d8faa1e0c5b
jiayezhao <412jiayezhao924@163.com>
2017-03-15 16:22:50 +0800

    First commit

---
+++ app/src/main/java/edu/xxz/com/coolweather/City.java
@@ -0,0 +1,46 @@
+package edu.xxz.com.coolweather;
+
+
+
+/**
+ * Created by azf on 17/3/15.
+ */
+
+public class City {
+    private int id;
+    private  String cityName;
+    private  int cityCode;
+    private  int provinceId;
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
